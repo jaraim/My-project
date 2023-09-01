ef17d959db385cc71e164ddefd50ca600acd083b
jaraim <117310975+jaraim@users.noreply.github.com>
2023-09-01 18:17:32 +0800

    Update VPS update and upgrade.sh

--- VPS update and upgrade.sh
+++ VPS update and upgrade.sh
@@ -1,26 +1,11 @@
-# 检查系统发行版
-distribution=$(cat /etc/os-release | awk -F'=' '{print $1}')
-
-# 检查可用更新
-if [ "${distribution}" == "CentOS" ]; then
-    available_updates=$(yum list available)
-elif [ "${distribution}" == "Ubuntu" ]; then
-    available_updates=$(apt list --upgradable)
+#!/bin/bash
+# 安装依赖工具
+if [[ $(command -v apt-get) ]]; then
+sudo apt update -y && sudo apt upgrade -y
+elif [[ $(command -v yum) ]]; then
+sudo yum update -y && sudo yum upgrade -y
+elif [[ $(command -v dnf) ]]; then
+sudo dnf update -y && sudo dnf upgrade -y
 else
-    echo "Unsupported distribution"
-    exit 1
-fi
-
-# 选择更新升级命令
-if [ -z "${available_updates}" ]; then
-    echo "No updates available"
-else
-    if [ "${distribution}" == "CentOS" ]; then
-        upgrade_command="yum update && yum upgrade"
-    elif [ "${distribution}" == "Ubuntu" ]; then
-        upgrade_command="apt update && apt upgrade"
-    fi
-
-    echo "Running ${upgrade_command}"
-    ${upgrade_command}
+    echo "不支持的操作系统" && exit 1
 fi

dd1f91a61e33cedade80b133a9a6a68f02a6dc39
jaraim <117310975+jaraim@users.noreply.github.com>
2023-09-19 01:17:02 +0800

    Update VPS_update&upgrade.sh

--- VPS_update&upgrade.sh
+++ VPS_update&upgrade.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # 安装依赖工具
 if [[ $(command -v apt-get) ]]; then
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update -y 
 elif [[ $(command -v yum) ]]; then
-sudo yum update -y && sudo yum upgrade -y
+sudo yum update -y 
 elif [[ $(command -v dnf) ]]; then
-sudo dnf update -y && sudo dnf upgrade -y
+sudo dnf update -y 
 else
     echo "不支持的操作系统" && exit 1
 fi

5462be6c30d904d07359dc77ed63d2e5000c254a
jaraim <117310975+jaraim@users.noreply.github.com>
2023-09-22 00:52:22 +0800

    Update VPS_update&upgrade.sh

--- VPS_update&upgrade.sh
+++ VPS_update&upgrade.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # 安装依赖工具
 if [[ $(command -v apt-get) ]]; then
-sudo apt update -y 
+sudo apt update -y && sudo apt upgrade -y
 elif [[ $(command -v yum) ]]; then
-sudo yum update -y 
+sudo yum update -y && sudo yum upgrade -y
 elif [[ $(command -v dnf) ]]; then
-sudo dnf update -y 
+sudo dnf update -y && sudo dnf upgrade -y
 else
     echo "不支持的操作系统" && exit 1
 fi
@@ -24,11 +24,11 @@ else
     fi
 fi
 
- # 检查pip3是否已安装
+# 检查pip3是否已安装
 if [ -x "$(command -v pip3)" ]; then
     echo "pip3已经安装"
     
-# 检查pip版本是否为最新，如果不是则进行升级
+    # 检查pip版本是否为最新，如果不是则进行升级
     if [ "$(pip3 --version | awk '{print $2}' | cut -d'.' -f1)" -lt 23 ]; then
         echo "pip版本不是最新版。正在升级..."
         sudo pip3 install --upgrade --force pip
@@ -36,22 +36,54 @@ if [ -x "$(command -v pip3)" ]; then
 else
     echo "pip3未安装。正在安装..."
     
-   
-if [ -x "$(command -v apt-get)" ]; then
+    if [ -x "$(command -v apt-get)" ]; then
         sudo apt-get update
         sudo apt-get install -y python3-pip
     
         sudo apt-get update
         sudo apt-get install -y
-else
+    else
         sudo yum update
         sudo yum install -y python3-pip
     
         sudo yum update
         sudo yum install -y python3-pip
+    fi
 fi
+
+# 检查git是否已安装
+if [ -x "$(command -v git)" ]; then
+    echo "git已安装"
+else
+    echo "git未安装。正在安装..."
+    if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get update
+        sudo apt-get install -y git
+    else
+        sudo yum update
+        sudo yum install -y git
+    fi
 fi
-# 检查Python 3和pip3是否安装成功
-echo "Python 3和pip3安装完成"
+
+# 检查docker-io、docker-ce和docker-compose是否已安装
+if [ -x "$(command -v docker)" ]; then
+    echo "docker已安装"
+else
+    echo "docker未安装。正在安装..."
+    if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get update
+        sudo apt-get install -y docker.io docker-ce docker-compose
+    else
+        sudo yum update
+        sudo yum install -y docker.io docker-ce docker-compose
+    fi
+fi
+
+# 检查Python3，pip3，git,docker,docker-ce,docker-compose是否安装成功
+echo "安装完成"
 python3 --version
 pip3 --version
+git --version
+docker --version
+docker-ce --version
+docker-compose --version

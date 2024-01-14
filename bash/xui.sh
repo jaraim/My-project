7cd02c816d7a9d77012518825ae039b07a63b23a
jaraim <117310975+jaraim@users.noreply.github.com>
2024-01-15 01:44:03 +0800

    Update and rename .sh to xui.sh

---
+++ bash/xui.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#Install Latest Stable Version of x-ui
+
+# 1. 获取系统架构信息
+osCheck=`uname -a`
+if [[ $osCheck =~ 'x86_64' ]];then
+    architecture="amd64"
+elif [[ $osCheck =~ 'arm64' ]] || [[ $osCheck =~ 'aarch64' ]];then
+    architecture="arm64"
+elif [[ $osCheck =~ 'armv7l' ]];then
+    architecture="armv7"
+elif [[ $osCheck =~ 'ppc64le' ]];then
+    architecture="ppc64le"
+elif [[ $osCheck =~ 's390x' ]];then
+    architecture="s390x"
+else
+    echo "Unsupported architecture, please refer to the official documentation and choose a supported system."
+    exit 1
+fi
+
+# 2. 检查安装模式，默认为"stable"
+if [[ ! ${INSTALL_MODE} ]]; then
+    INSTALL_MODE="stable"
+fi
+
+# 3. 从Github API获取最新版本信息
+VERSION=$(curl -s "https://api.github.com/repos/vaxilu/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
+# 4. 拼接普通版本下载链接
+FILE_URL="https://github.com/vaxilu/x-ui/releases/download/${VERSION}/x-ui-linux-${architecture}.tar.gz"
+
+# 5. 如果已存在相同版本的文件，则跳过下载
+if [ -f "x-ui-linux-${architecture}.tar.gz" ]; then
+    echo "The file exists and will skip the download."
+else
+    # 6. 下载最新的 x-ui 安装包（如果不存在）
+    echo "Downloading x-ui-${VERSION} installation package"
+    curl -L ${FILE_URL} -o x-ui-linux-${architecture}.tar.gz
+    # 如果下载失败，停止执行脚本
+    if [ "$?" != "0" ]; then
+        echo "Failed to download, please check your network connection."
+        exit 1
+    fi
+fi
+
+# 7. 解压安装包并开始执行软件安装
+tar zxvf x-ui-linux-${architecture}.tar.gz && cd x-ui && ./install.sh

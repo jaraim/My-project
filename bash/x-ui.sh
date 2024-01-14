c5a76d7915cd94c4faf77870a592c751a51a15ae
jaraim <117310975+jaraim@users.noreply.github.com>
2024-01-15 01:57:59 +0800

    Update x-ui.sh

--- bash/x-ui.sh
+++ bash/x-ui.sh
@@ -14,7 +14,7 @@ elif [[ $osCheck =~ 'ppc64le' ]];then
 elif [[ $osCheck =~ 's390x' ]];then
     architecture="s390x"
 else
-    echo "Unsupported architecture, please refer to the official documentation and choose a supported system."
+    echo "不支持的架构，请参考官方文档选择支持的系统."
     exit 1
 fi
 
@@ -31,17 +31,17 @@ FILE_URL="https://github.com/vaxilu/x-ui/releases/download/${VERSION}/x-ui-linux
 
 # 5. 如果已存在相同版本的文件，则跳过下载
 if [ -f "x-ui-linux-${architecture}.tar.gz" ]; then
-    echo "The file exists and will skip the download."
+    echo "文件存在，将跳过下载."
 else
     # 6. 下载最新的 x-ui 安装包（如果不存在）
-    echo "Downloading x-ui-${VERSION} installation package"
+    echo "正在下载 x-ui-${VERSION} 安装包"
     curl -L ${FILE_URL} -o x-ui-linux-${architecture}.tar.gz
     # 如果下载失败，停止执行脚本
     if [ "$?" != "0" ]; then
-        echo "Failed to download, please check your network connection."
+        echo "下载失败，请检查您的网络连接."
         exit 1
     fi
 fi
 
 # 7. 解压安装包并开始执行软件安装
-tar zxvf x-ui-linux-${architecture}.tar.gz && cd x-ui && ./install.sh
+tar zxvf x-ui-linux-${architecture}.tar.gz && cd x-ui && ./x-ui.sh

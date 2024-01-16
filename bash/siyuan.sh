efca3680e8c3ff1cf9469f91bca9dae8864b4061
jaraim <117310975+jaraim@users.noreply.github.com>
2024-01-17 07:05:14 +0800

    Update siyuan.sh

--- bash/siyuan.sh
+++ bash/siyuan.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
+# 输入 accessAuthCode
+echo -n "请输入accessAuthCode: "
+read accessAuthCode
+
 # 下载 compose.yml 文件
 curl -o compose.yml -LJO https://raw.githubusercontent.com/jaraim/My-project/main/docker/siyuan/compose.yml
 
-# 使用 nano 编辑器打开 compose.yml 文件进行编辑
-nano compose.yml
+# 使用 sed 命令在 compose.yml 中替换 accessAuthCode
+sed -i "s|accessAuthCode=xxx|accessAuthCode=${accessAuthCode}|g" compose.yml
 
 # 使用 docker-compose 启动 Docker 服务
 docker-compose up -d

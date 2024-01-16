d6c60b7d10d09124ca040614bafc776dcf80699a
jaraim <117310975+jaraim@users.noreply.github.com>
2024-01-17 07:46:33 +0800

    Update Ssl.sh

--- bash/Ssl.sh
+++ bash/Ssl.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
+# 更新系统并安装必要的工具
+apt update -y && apt install -y curl socat wget
+
+# 下载并安装acme.sh
+curl https://get.acme.sh | sh
+
+# 打印选择项
 echo "请选择一个操作:"
 echo "  1) 申请SSL"
 echo "  2) 下载SSL"
 
+# 读取用户选择
 read -p "选择一个项目并按回车: " choice
 
 case $choice in
+
   1)
+    # 如果用户选择申请SSL
     echo -n "输入您的邮箱: "
     read email
     echo -n "输入您的域名: "
     read domain
-    curl https://get.acme.sh | sh
     ~/.acme.sh/acme.sh --register-account -m $email
     ~/.acme.sh/acme.sh --issue -d $domain --standalone
     ;;
+    
   2)
+    # 如果用户选择下载SSL
     echo -n "输入您的域名: "
     read domain
     echo -n "请输入私钥文件的保存位置: "
@@ -25,7 +36,10 @@ case $choice in
     read certFilePath
     ~/.acme.sh/acme.sh --install-cert -d $domain --key-file $keyFilePath --fullchain-file $certFilePath
     ;;
+    
   *)
+    # 如果用户输入了一个未知选项
     echo "未知选项: $choice"
     ;;
+    
 esac
